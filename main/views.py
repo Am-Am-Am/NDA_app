7af7afa79af226caac6a06234a65972d6db29d96
Am-Am-Am <amur_almukhametov@mail.ru>
2024-10-07 12:43:16 +0500

    first

---
+++ main/views.py
@@ -0,0 +1,50 @@
+from typing import Any
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+
+def index(request):
+
+
+    context = {
+        'title': 'Home - Главная',
+        'content': "Магазин мебели HOME",
+    }
+
+    return render(request, 'main/index.html', context)
+   
+
+def about(request):
+    context = {
+        'title': 'Home - О нас',
+        'content': "О нас",
+        'text_on_page': "Текст о том почему этот магазин такой классный, и какой хороший товар."
+    }
+
+    return render(request, 'main/about.html', context)
+    
+
+# Create your views here.
+# class IndexView(TemplateView):
+
+#     template_name ='main/index.html'
+
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['title'] = 'Home - Главная'
+#         context['content'] = "Магазин мебели HOME"
+#         # context['categories'] = Categories.objects.all()
+#         return context
+
+
+
+# class AboutView(TemplateView):
+#     template_name ='main/about.html'
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['title'] = 'Home - О нас'  
+#         context['content'] = "О нас"
+#         context['text_on_page'] = "Текст о том почему этот магазин такой классный, и какой хороший товар."                  
+
+#         return context
